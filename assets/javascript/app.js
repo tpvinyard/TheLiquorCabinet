9570fd2ca94a88d5bb70dfb071fc1a83c8059939
Dibora D <diborad@yahoo.com>
2019-09-14 11:36:40 -0500

    added a clikc event to remove added ingredientson a time instead of all the indredients

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,79 +1,70 @@
 $(document).ready(function() {
-    let returnedDrinks = {};
-    let returnedDrinksArray = '';
-    let searchValue = '';
-    // for testing purposes
-    let additionalIngredientsArray = [];
-    let ingredientCounter = 0;
+  let returnedDrinks = {};
+  let returnedDrinksArray = "";
+  let searchValue = "";
+  // for testing purposes
+  let additionalIngredientsArray = [];
+  let ingredientCounter = 0;
 
-    $("#submit-button").on("click", function() {
-        let inputAlcohol = $('#searchAlcohol').val();
+  $("#submit-button").on("click", function() {
+    let inputAlcohol = $("#searchAlcohol").val();
 
-        let ingredientArrayToText = additionalIngredientsArray.join('&i=');
-        
+    let ingredientArrayToText = additionalIngredientsArray.join("&i=");
 
-        if (!inputAlcohol == '') {
-            ingredientArrayToText = '&i=' + ingredientArrayToText;
-            
-        }
+    if (!inputAlcohol == "") {
+      ingredientArrayToText = "&i=" + ingredientArrayToText;
+    }
 
-        let queryURL = "https://www.thecocktaildb.com/api/json/v2/8673533/filter.php?i=" + inputAlcohol + ingredientArrayToText;
+    let queryURL = "https://www.thecocktaildb.com/api/json/v2/8673533/filter.php?i=" + inputAlcohol + ingredientArrayToText;
 
+    $.ajax({
+      url: queryURL,
+      method: "GET"
+    }).then(function(response) {
+      returnedDrinks = response;
+      returnedDrinksArray = getDrinkIDArray(returnedDrinks);
+      for (let i = 0; i < returnedDrinksArray.length; i++) {
+        let queryDrinksUrl = "https://www.thecocktaildb.com/api/json/v2/8673533/lookup.php?i=" + returnedDrinksArray[i];
         $.ajax({
-        url: queryURL,
-        method: "GET"
-        })
-        .then(function(response) {
-            returnedDrinks = response;
-            returnedDrinksArray = getDrinkIDArray(returnedDrinks);
-            for (let i = 0; i<returnedDrinksArray.length; i++) {
-                let queryDrinksUrl = "https://www.thecocktaildb.com/api/json/v2/8673533/lookup.php?i=" + returnedDrinksArray[i];
-                $.ajax({
-                    url: queryDrinksUrl,
-                    method: "GET"
-                })
-                .then(function(response){
-                    console.log(response)
-                })
-            }
+          url: queryDrinksUrl,
+          method: "GET"
+        }).then(function(response) {
+          console.log(response);
         });
-
+      }
     });
+  });
 
-    function getDrinkIDArray(response) {
-        let drinkIDArray = [];
-        for (let i = 0; i < response.drinks.length; i++) {
-            drinkIDArray.push(response.drinks[i].idDrink);
-        }
-        return drinkIDArray;
+  function getDrinkIDArray(response) {
+    let drinkIDArray = [];
+    for (let i = 0; i < response.drinks.length; i++) {
+      drinkIDArray.push(response.drinks[i].idDrink);
     }
+    return drinkIDArray;
+  }
 
+  function addIngredient() {
+    let additionalIngredients = $("<button>");
+    additionalIngredients.text(searchValue);
+    additionalIngredients.attr("class", "ingredient");
+    additionalIngredients.attr("data-position", ingredientCounter);
+    additionalIngredients.addClass("btn");
+    additionalIngredients.addClass("btn-secondary");
+    additionalIngredientsArray.push(searchValue);
+    $("#ingredientContainer").append(additionalIngredients);
+    ingredientCounter++;
+  }
+    $("#add-ingredients").on("click", function() {
+      searchValue = $("#searchAlcohol")
+        .val()
+        .trim();
 
-
-    function addIngredient() {
-        let additionalIngredients = $('<button>');
-        additionalIngredients.text(searchValue);
-        additionalIngredients.attr('class', 'ingredient');
-        additionalIngredients.attr('data-position', ingredientCounter);
-        additionalIngredients.addClass('btn');
-        additionalIngredients.addClass('btn-secondary');
-        additionalIngredientsArray.push(searchValue);
-        $('#ingredientContainer').append(additionalIngredients); 
-        ingredientCounter++;
-    }
+      if (!searchValue == "") {
+        addIngredient();
+      }
+    });
     
-    $('#add-ingredients').on('click', function(){
-        searchValue = $('#searchAlcohol').val().trim();
-
-        if (!searchValue == '') {
-            addIngredient();
-        }
-    })
-
-    
-    $('.ingredient').on('click', function(){
-        $(this).remove();
-    })
-    
-
+  $(document).on("click", ".ingredient", function() {
+    $(this).remove();
+  });
 });
