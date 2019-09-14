049fc16ce4e4350841caf9be5ef3f774734c1a2c
Dibora D <diborad@yahoo.com>
2019-09-14 13:04:16 -0500

    js empty search value once its clicke

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -11,10 +11,9 @@ $(document).ready(function() {
 
     let ingredientArrayToText = additionalIngredientsArray.join("&i=");
 
-        if (!inputAlcohol == '') {
-            ingredientArrayToText = ',' + ingredientArrayToText;
-            
-        }
+    if (!inputAlcohol == "") {
+      ingredientArrayToText = "," + ingredientArrayToText;
+    }
 
     let queryURL = "https://www.thecocktaildb.com/api/json/v2/8673533/filter.php?i=" + inputAlcohol + ingredientArrayToText;
 
@@ -27,54 +26,34 @@ $(document).ready(function() {
       for (let i = 0; i < returnedDrinksArray.length; i++) {
         let queryDrinksUrl = "https://www.thecocktaildb.com/api/json/v2/8673533/lookup.php?i=" + returnedDrinksArray[i];
         $.ajax({
-
-        url: queryURL,
-        method: "GET"
-        })
-        .then(function(response) {
-            returnedDrinks = response;
-            returnedDrinksArray = getDrinkIDArray(returnedDrinks);
-            console.log(queryURL)
-            for (let i = 0; i<returnedDrinksArray.length; i++) {
-                let queryDrinksUrl = "https://www.thecocktaildb.com/api/json/v2/8673533/lookup.php?i=" + returnedDrinksArray[i];
-                $.ajax({
-                    url: queryDrinksUrl,
-                    method: "GET"
-                })
-                .then(function(response){
-                    console.log(queryDrinksUrl)
-                    console.log(response)
-                })
-            }
+          url: queryURL,
+          method: "GET"
+        }).then(function(response) {
+          returnedDrinks = response;
+          returnedDrinksArray = getDrinkIDArray(returnedDrinks);
+          console.log(queryURL);
+          for (let i = 0; i < returnedDrinksArray.length; i++) {
+            let queryDrinksUrl = "https://www.thecocktaildb.com/api/json/v2/8673533/lookup.php?i=" + returnedDrinksArray[i];
+            $.ajax({
+              url: queryDrinksUrl,
+              method: "GET"
+            }).then(function(response) {
+              console.log(queryDrinksUrl);
+              console.log(response);
+            });
+          }
         });
       }
     });
   });
-  
-    function getDrinkIDArray(response) {
-        let drinkIDArray = [];
-        for (let i = 0; i < response.drinks.length; i++) {
-            drinkIDArray.push(response.drinks[i].idDrink);
-        }
-        return drinkIDArray;
-    }
 
-
-
-    function addIngredient() {
-        let additionalIngredients = $('<button>');
-        additionalIngredients.text(searchValue);
-        additionalIngredients.addClass('ingredient');
-        additionalIngredients.attr('data-position', ingredientCounter);
-        additionalIngredients.addClass('btn');
-        additionalIngredients.addClass('btn-secondary');
-        additionalIngredientsArray.push(searchValue);
-        $('#ingredientContainer').append(additionalIngredients); 
-        ingredientCounter++;
+  function getDrinkIDArray(response) {
+    let drinkIDArray = [];
+    for (let i = 0; i < response.drinks.length; i++) {
+      drinkIDArray.push(response.drinks[i].idDrink);
     }
     return drinkIDArray;
   }
-
   function addIngredient() {
     let additionalIngredients = $("<button>");
     additionalIngredients.text(searchValue);
@@ -86,26 +65,24 @@ $(document).ready(function() {
     $("#ingredientContainer").append(additionalIngredients);
     ingredientCounter++;
   }
-    $("#add-ingredients").on("click", function() {   // 
-      searchValue = $("#searchAlcohol")
-        .val()
-        .trim();
+  $("#add-ingredients").on("click", function() {
+    //
+    searchValue = $("#searchAlcohol")
+      .val()
+      .trim();
 
-      if (!searchValue == "") {
-        addIngredient();
-      }
-    });
-    
-    $(document).on('click', '.ingredient', function(){
-        event.preventDefault();
-        let position = $(this).data('position');
-        additionalIngredientsArray.splice(position, 1);
-        $(this).remove();
-        console.log(additionalIngredientsArray)
-        
-    })
-       
-    
-    
+    if (!searchValue == "") {
+      addIngredient();
+    }
+  });
 
+  $(document).on("click", ".ingredient", function() {
+    event.preventDefault();
+    let position = $(this).data("position");
+    additionalIngredientsArray.splice(position, 1);
+    $(this).remove();
+    console.log(additionalIngredientsArray);
+  });
+
+  // $(searchValue).empty();
 });
